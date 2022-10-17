63fb3dab6391c7e7cf63c58aad5a7f1887ff83e4
stepik0rn <eklmn@my.com>
2022-10-17 11:20:28 +0300

    Add 6th lesson solution

---
+++ 6.py
@@ -0,0 +1,42 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+
+try: 
+   
+    link = "http://suninjuly.github.io/registration2.html"
+    browser = webdriver.Chrome()
+    browser.get(link)
+
+    # Ваш код, который заполняет обязательные поля
+    name = (By.XPATH, "//div[label='First name*']/input")
+    lname = (By.XPATH, "//div[label='Last name*']/input")
+    email = (By.XPATH, "//div[label='Email*']/input")
+    
+    els = [name, lname, email]
+
+    for el_selector in els:
+        el = browser.find_element(*el_selector)
+        el.send_keys("test")
+    
+    # Отправляем заполненную форму
+    button = browser.find_element(By.CSS_SELECTOR, "button.btn")
+    button.click()
+
+    # Проверяем, что смогли зарегистрироваться
+    # ждем загрузки страницы
+    time.sleep(1)
+
+    # находим элемент, содержащий текст
+    welcome_text_elt = browser.find_element(By.TAG_NAME, "h1")
+    # записываем в переменную welcome_text текст из элемента welcome_text_elt
+    welcome_text = welcome_text_elt.text
+
+    # с помощью assert проверяем, что ожидаемый текст совпадает с текстом на странице сайта
+    assert "Congratulations! You have successfully registered!" == welcome_text
+
+finally:
+    # ожидание чтобы визуально оценить результаты прохождения скрипта
+    time.sleep(10)
+    # закрываем браузер после всех манипуляций
+    browser.quit()
